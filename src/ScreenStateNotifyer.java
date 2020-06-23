a9cbc27a12fb961eb3db727b7a704cc065e18fdf
Niria <andreamoschetto99@gmail.com>
2020-06-23 22:28:24 +0200

    add ScreenStateNotifyer class

---
+++ src/ScreenStateNotifyer.java
@@ -0,0 +1,16 @@
+public class ScreenStateNotifyer implements EventObserver{
+    private static ScreenStateNotifyer instance;
+    private ScreenStateNotifyer(){}
+
+    public static ScreenStateNotifyer ScreenStateNotifyer(){
+        if(null == instance)
+            instance = new ScreenStateNotifyer();
+        return instance;
+    }
+    @Override
+    public void update() {
+        String stateName = Player.getInstance().getStateName();
+        System.out.println("[NOTIFICA] Valoroso ha appena raggiunto lo stato: " + stateName);
+    }
+    
+}
